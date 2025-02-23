4f5ae41d7769faad4ed8c5de9d077e23ff8bf07e
SpikyShark <spikyshark04@gmail.com>
2025-02-23 07:04:25 -0500

    boss 3

---
+++ Assets/Scripts/Boss 3/BossMain3.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using System.Collections;
+
+public class BossMain3 : MonoBehaviour
+{
+    public float moveSpeed = 12.0f;
+    public Transform player; 
+    public float moveRadius = 10f; 
+    private Vector3 targetPosition; 
+    public bool arrived = false;
+    public bool invincible = false;
+
+    void Start()
+    {
+        StartCoroutine(MoveAndShootRoutine());
+    }
+
+    private IEnumerator MoveAndShootRoutine()
+    {
+        while (true)
+        {
+            targetPosition = GetRandomPositionAroundPlayer();
+            yield return StartCoroutine(MoveToTarget(targetPosition));
+            yield return new WaitForSeconds(6.0f);
+        }
+    }
+
+    private Vector3 GetRandomPositionAroundPlayer()
+    {
+        Vector2 randomDirection = Random.insideUnitCircle * moveRadius;
+        Vector3 randomPosition = new Vector3(randomDirection.x, 0f, randomDirection.y);
+        randomPosition += player.position; 
+        arrived = false;
+        return randomPosition;
+    }
+
+    private IEnumerator MoveToTarget(Vector3 target)
+    {
+        invincible = false;
+        while (Vector3.Distance(transform.position, target) > 0.05f) 
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+
+            Vector3 direction = target - transform.position;
+            if (direction != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+            }
+
+            yield return null;
+        }
+
+        arrived = true;
+    }
+}
