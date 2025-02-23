d68bc28e841897ee622a4c92d06e4650f1e9f726
IcedQ6 <burningbabystudios@gmail.com>
2025-02-22 23:20:58 -0500

    fuck im sleepy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,14 +6,18 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public Vector2 spawnPos;
-    public Vector2 endPos;
+    //public Vector2 spawnPos;
+    //public Vector2 endPos;
 
-    public bool useMoreThanTwoPoints = false;
+    //public bool useMoreThanTwoPoints = false;
 
+    [Header("Will be overwritten by WaveManager.")]
     public Vector2[] possitionArray;
     private int possitionArrayIndex = 0;
+    public int level;
+    public int wave;
 
+    [Header("Adjustable parameters")]
     public float enemyMoveSpeed = 5.0f;
 
     private bool isMovingToEnd = true;
@@ -21,69 +25,69 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (!useMoreThanTwoPoints)
-        {
-            if (spawnPos == null || endPos == null)
-            {
-                spawnPos = new Vector2(-10, -10);
-                endPos = new Vector2(10, 10);
-            }
-            transform.position = new Vector3(spawnPos.x, 0f, spawnPos.y);
-        }
-        else
-        {
+        //if (!useMoreThanTwoPoints)
+        //{
+        //    if (spawnPos == null || endPos == null)
+        //    {
+        //        spawnPos = new Vector2(-10, -10);
+        //        endPos = new Vector2(10, 10);
+        //    }
+        //    transform.position = new Vector3(spawnPos.x, 0f, spawnPos.y);
+        //}
+        //else
+        //{
             transform.position = new Vector3(possitionArray[0].x, 0f, possitionArray[0].y);
-        }
+        //}
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (useMoreThanTwoPoints == false)
-        {
-            movingSpawnToEnd();
-        }
-        else
-        {
+        //if (useMoreThanTwoPoints == false)
+        //{
+        //    movingSpawnToEnd();
+        //}
+        //else
+        //{
             moveBetweenPoints();
-        }
+        //}
     }
 
-    private bool checkDistanceToDestination()
-    {
-        Vector2 currentPos;
-        currentPos = new Vector2(transform.position.x, transform.position.z);
+    //private bool checkDistanceToDestination()
+    //{
+    //    Vector2 currentPos;
+    //    currentPos = new Vector2(transform.position.x, transform.position.z);
 
-        float distanceToDestination;
-        if (isMovingToEnd == true) distanceToDestination = Mathf.Abs(currentPos.x - endPos.x) + Mathf.Abs(currentPos.y - endPos.y);
-        else distanceToDestination = (currentPos.x - spawnPos.x) + (currentPos.y - spawnPos.y);
+    //    float distanceToDestination;
+    //    if (isMovingToEnd == true) distanceToDestination = Mathf.Abs(currentPos.x - endPos.x) + Mathf.Abs(currentPos.y - endPos.y);
+    //    else distanceToDestination = (currentPos.x - spawnPos.x) + (currentPos.y - spawnPos.y);
 
-        if (distanceToDestination < 0.5f) return true;
-        else return false;
-    }
+    //    if (distanceToDestination < 0.5f) return true;
+    //    else return false;
+    //}
 
-    private void swapMovingToEnd ()
-    {
-        isMovingToEnd = !isMovingToEnd;
-    }
+    //private void swapMovingToEnd ()
+    //{
+    //    isMovingToEnd = !isMovingToEnd;
+    //}
 
-    private void movingSpawnToEnd()
-    {
-        if (checkDistanceToDestination() == true) swapMovingToEnd();
+    //private void movingSpawnToEnd()
+    //{
+    //    if (checkDistanceToDestination() == true) swapMovingToEnd();
 
-        if (isMovingToEnd == true)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3(endPos.x, 0f, endPos.y), enemyMoveSpeed * Time.deltaTime);
-        }
-        else
-        {
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3(spawnPos.x, 0f, spawnPos.y), enemyMoveSpeed * Time.deltaTime);
-        }
-    }
+    //    if (isMovingToEnd == true)
+    //    {
+    //        transform.position = Vector3.MoveTowards(transform.position, new Vector3(endPos.x, 0f, endPos.y), enemyMoveSpeed * Time.deltaTime);
+    //    }
+    //    else
+    //    {
+    //        transform.position = Vector3.MoveTowards(transform.position, new Vector3(spawnPos.x, 0f, spawnPos.y), enemyMoveSpeed * Time.deltaTime);
+    //    }
+    //}
 
     // Moves from index 0 to 1 then n to n+1 and back to 1.
-    // The enemy does not return to 0 in this case.
+    // The enemy does not return to 0 in this system.
     private void moveBetweenPoints()
     {
         possitionArrayIndex = distanceToNextIndex();
@@ -101,6 +105,8 @@ public class Enemy : MonoBehaviour
 
     }
 
+    // Returns n+1 of the enemy is within .5 units of the next point,
+    // which will then cause them to move to the following point.
     private int distanceToNextIndex()
     {
         Vector2 currentPos;
--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
-    // An array of each enemy in the wave
-    
-    // First value holds the index with a delay before it spawns,
-    // Second value is the length of that delay.
-    public Vector2[] delayList;
+    public int levelNum;
+    public int waveNum;
+    public GameObject[] enemies; // Contains ver 1, 2, 3
+    public Mesh[] enemyMeshs; // Contains the version of the enemies for the level
+
+    [System.Serializable]
+    public class Enemy
+    {
+        public int type; // Enemy 1, 2, or 3
+        public Vector2[] spawns; // Points enemy will travel
+        public int repeat; // How many times this specific instance spawns (0 is default)
+        public float delay; // How long enemy will refrain from spawning
+    }
+
+    public Enemy[] enemy;
 
 
     // Start is called before the first frame update
